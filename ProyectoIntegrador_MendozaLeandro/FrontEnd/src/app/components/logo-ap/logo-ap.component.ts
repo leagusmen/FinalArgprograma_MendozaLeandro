ecb78549e2a8d78ac22f6039562cfd1cbc5301af
leagusmen <leagusmen@hotmail.com>
2022-08-19 12:32:35 -0300

    a

--- ProyectoIntegrador_MendozaLeandro/FrontEnd/src/app/components/logo-ap/logo-ap.component.ts
+++ ProyectoIntegrador_MendozaLeandro/FrontEnd/src/app/components/logo-ap/logo-ap.component.ts
@@ -8,12 +8,9 @@ import { Router } from '@angular/router';
 })
 export class LogoAPComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-  login(){
-    this.router.navigate(['/login'])
-  }
+  
 }
